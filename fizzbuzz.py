19fe0f1f833acdfa520ce32b8ca1c466202fe095
Morgan Walker <mpwalker517@gmail.com>
2023-07-23 19:31:11 -0400

    fizzbuzzrepo

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,11 @@
 for number in range(1,100):
-    if number%5 == 0 and number%3 == 0:
+    if number %5 == 0 and number %3 == 0:
         print("FizzBuzz")
-    elif number%3 == 0:
+    elif number %3 == 0:
         print("Fizz")
-    elif number%5 == 0:
+    elif number %5 == 0:
         print("Buzz")
     else:
         print(number)
+
 
